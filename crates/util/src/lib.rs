fe2a2dc27f60d2854bd6241320ba2ee8f774daae
Jorge López <jorgekq@hotmail.es>
2023-12-01 21:51:49 +0100

    Creates path_to_lines utility function

--- crates/util/src/lib.rs
+++ crates/util/src/lib.rs
@@ -1,5 +1,15 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
+use std::path::Path;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn path_to_lines(file_name: &Path) -> anyhow::Result<Lines<BufReader<File>>> {
+    let file = File::open(file_name)?;
+    let file_reader = BufReader::new(file);
+    Ok(file_reader.lines())
 }
 
 #[cfg(test)]
